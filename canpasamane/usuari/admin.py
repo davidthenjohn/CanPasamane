58a63cded9da5f6cc063838532d998f9bc8e74d2
david <davidjuannn@gmail.com>
2019-05-09 21:03:29 +0200

    canvis a admin de ususari i afegit el helper

--- canpasamane/usuari/admin.py
+++ canpasamane/usuari/admin.py
@@ -2,5 +2,24 @@ from django.contrib import admin
 from django.contrib.auth.models import Group
 from django.contrib.auth.admin import UserAdmin as modelAdmin
 from usuari.models import User
+from .form import UserChangeForm,UserCreationForm
 
 # Register your models here.
+class UserAdmin(modelAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    list_display = ("nom_user", "email" , "is_admin")
+    list_filter = ("is_admin",)
+    fieldsets = (
+        (None, {"fields": ("nom_user", "email", "password", "telefon")}),
+        ("Personal info", {"fields": ("cognoms_user",)}),
+        ("Permissions", {"fields": ("is_admin",)}),
+    )
+
+    search_fields = ("nom_user",)
+    ordering = ("nom_user",)
+    filter_horizontal = ()
+
+admin.site.register(User, UserAdmin)
+admin.site.unregister(Group)
